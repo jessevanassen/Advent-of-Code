d9e714320ffc219043bfa44cd7e37b89683f67f7
Jesse van Assen <jesse.v.assen@gmail.com>
2020-12-20 18:53:52 +0100

    Day 20: add solution for part 2

--- src/solutions/20/index.ts
+++ src/solutions/20/index.ts
@@ -1,9 +1,22 @@
 import { remove } from '../../lib';
-import { pipe } from '../../lib/fp';
+import { eq, pipe } from '../../lib/fp';
 import { collectToArray, map, range, zipWithIndex } from '../../lib/fp/generators';
 import { readBlocksFromStdin } from '../../lib/fs';
 
-const SIZE = 10;
+const TILE_SIZE = 10;
+
+const seaMonster = [
+	'                  # ',
+	'#    ##    ##    ###',
+	' #  #  #  #  #  #   ',
+]
+	.map(line => line
+		.split('')
+		.map(eq('#')));
+const seamonsterSize = seaMonster
+	.flat()
+	.filter(Boolean)
+	.length;
 
 interface Tile {
 	id: number;
@@ -17,17 +30,27 @@ interface TileBorders {
 	left: number;
 }
 
+interface Image {
+	size: number;
+	get(x: number, y: number): boolean;
+}
+
 const tiles = parseTiles();
+const imageSize = Math.sqrt(tiles.length);
 
 const configurations = validConfigurations(tiles);
-{
-	const size = imageSize(tiles);
-	console.log('Part 1:', configurations[0][0].id * configurations[0][size - 1].id * configurations[0][tiles.length - size].id * configurations[0][tiles.length - 1].id);
+console.log('Part 1:', configurations[0][0].id * configurations[0][imageSize - 1].id * configurations[0][tiles.length - imageSize].id * configurations[0][tiles.length - 1].id);
+
+for (const configuration of configurations) {
+	const img = Image(configuration);
+	const count = countSeaMonsters(img);
+
+	if (count > 0) {
+		console.log('Part 2: ', countTrue(img) - count * seamonsterSize);
+	}
 }
 
 function validConfigurations(tiles: Tile[]): Tile[][] {
-	const size = imageSize(tiles);
-
 	const tileVariants = new Map<Tile, Tile[]>(
 		tiles.map(tile => [tile, variants(tile)]));
 	const variantBorders = new Map<Tile, TileBorders>();
@@ -42,8 +65,8 @@ function validConfigurations(tiles: Tile[]): Tile[][] {
 			return [acc];
 		}
 
-		const x = acc.length % size,
-			y = ~~(acc.length / size);
+		const x = acc.length % imageSize,
+			y = ~~(acc.length / imageSize);
 
 		const result: Tile[][] = [];
 
@@ -51,8 +74,8 @@ function validConfigurations(tiles: Tile[]): Tile[][] {
 			for (const nextConfiguration of tileVariants.get(next)!) {
 				const { left, top } = variantBorders.get(nextConfiguration)!;
 				if (
-					(x === 0 || left === variantBorders.get((acc[y * size + (x - 1)]))!.right) &&
-					(y === 0 || top ===  variantBorders.get((acc[(y - 1) * size + x]))!.bottom)
+					(x === 0 || left === variantBorders.get((acc[y * imageSize + (x - 1)]))!.right) &&
+					(y === 0 || top ===  variantBorders.get((acc[(y - 1) * imageSize + x]))!.bottom)
 				) {
 					result.push(..._possibleGrids(remove(tileIndex, deck), [...acc, nextConfiguration]));
 				}
@@ -75,9 +98,9 @@ function parseTiles(): Tile[] {
 		const [header, ...rows] = tileData.split('\n');
 		const id = Number.parseInt(header.match(/^Tile (\d+):$/)![1]);
 
-		const rowBytes = new Uint16Array(SIZE);
-		for (let y = 0; y < SIZE; y++) {
-			for (let x = 0; x < SIZE; x++) {
+		const rowBytes = new Uint16Array(TILE_SIZE);
+		for (let y = 0; y < TILE_SIZE; y++) {
+			for (let x = 0; x < TILE_SIZE; x++) {
 				rowBytes[y] |= (rows[y].charAt(x) === '#' ? 1 : 0) << x;
 			}
 		}
@@ -90,12 +113,12 @@ function parseTiles(): Tile[] {
 }
 
 function borders(tile: Tile): TileBorders {
-	const indices = [...range(SIZE)];
+	const indices = [...range(TILE_SIZE)];
 
 	return {
 		top: toNumber(indices.map(x => tile.get(x, 0))),
-		right: toNumber(indices.map(y => tile.get(SIZE - 1, y))),
-		bottom: toNumber(indices.map(x => tile.get(x, SIZE - 1))),
+		right: toNumber(indices.map(y => tile.get(TILE_SIZE - 1, y))),
+		bottom: toNumber(indices.map(x => tile.get(x, TILE_SIZE - 1))),
 		left: toNumber(indices.map(y => tile.get(0, y))),
 	};
 
@@ -116,7 +139,7 @@ function variants(tile: Tile): Tile[] {
 function rotateCW({ id, get }: Tile): Tile {
 	return {
 		get(x, y) {
-			const h = ((SIZE - 1) / 2);
+			const h = ((TILE_SIZE - 1) / 2);
 			const _x = (y - h) * -1 + h;
 			const _y = (x - h) + h;
 			return get(_x, _y);
@@ -128,12 +151,50 @@ function rotateCW({ id, get }: Tile): Tile {
 function flipHorizontal({ id, get }: Tile): Tile {
 	return {
 		get(x, y) {
-			return get(SIZE - 1 - x, y);
+			return get(TILE_SIZE - 1 - x, y);
 		},
 		id,
 	};
 }
 
-function imageSize(tiles: Tile[]): number {
-	return Math.sqrt(tiles.length);
+function Image(tiles: Tile[]): Image {
+	const size = TILE_SIZE - 2;
+	return {
+		size: size * imageSize,
+		get(x, y) {
+			const i = ~~(x / size) + ~~(y / size) * imageSize;
+			const tile = tiles[i];
+			return tile.get(
+				(x % size) + 1,
+				(y % size) + 1);
+		},
+	};
+}
+
+function countSeaMonsters(image: Image): number {
+	let result = 0;
+
+	for (const y of range(image.size - seaMonster.length)) {
+		for (const x of range(image.size - seaMonster[0].length)) {
+			if ([...range(seaMonster.length)]
+				.every(_y => [...range(seaMonster[0].length)]
+					.every(_x => !seaMonster[_y][_x] || image.get(x + _x, y + _y)))) {
+				result++;
+			}
+		}
+	}
+
+	return result;
+}
+
+function countTrue(image: Image): number {
+	let result = 0;
+	for (const x of range(image.size)) {
+		for (const y of range(image.size)) {
+			if (image.get(x, y)) {
+				result++;
+			}
+		}
+	}
+	return result;
 }
