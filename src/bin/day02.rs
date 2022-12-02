8a0b338a4a2b98dfaf4bbda4ed67d0f710c5ee5f
Jesse van Assen <jesse.v.assen@gmail.com>
2022-12-02 21:34:11 +0100

    Day 02: Rewrite solution to matches on the turn patterns

--- src/bin/day02.rs
+++ src/bin/day02.rs
@@ -1,187 +1,46 @@
-use std::{io::stdin, str::FromStr};
+use std::io::stdin;
 
-type Score = u8;
-
-#[derive(PartialEq, Eq, Clone, Copy, Debug)]
-enum Outcome {
-	Win,
-	Lose,
-	Draw,
+fn score(turn: &str) -> Result<u32, String> {
+	Ok(match turn {
+		"B X" => 1, // Paper    - Rock     : Lose
+		"C Y" => 2, // Scissors - Paper    : Lose
+		"A Z" => 3, // Rock     - Scissors : Lose
+		"A X" => 4, // Rock     - Rock     : Draw
+		"B Y" => 5, // Paper    - Paper    : Draw
+		"C Z" => 6, // Scissors - Scissors : Draw
+		"C X" => 7, // Scissors - Rock     : Win
+		"A Y" => 8, // Rock     - Paper    : Win
+		"B Z" => 9, // Paper    - Scissors : Win
+		_ => return Err(format!("Invalid pattern: '{turn}'")),
+	})
 }
 
-impl Outcome {
-	fn score(&self) -> Score {
-		match self {
-			Outcome::Win => 6,
-			Outcome::Draw => 3,
-			Outcome::Lose => 0,
-		}
-	}
-}
-
-impl FromStr for Outcome {
-	type Err = ();
-
-	fn from_str(s: &str) -> Result<Self, Self::Err> {
-		use Outcome::*;
-		Ok(match s {
-			"X" => Lose,
-			"Y" => Draw,
-			"Z" => Win,
-			_ => return Err(()),
-		})
-	}
-}
-
-#[derive(PartialEq, Eq, Clone, Copy, Debug)]
-enum Move {
-	Rock,
-	Paper,
-	Scissors,
-}
-
-impl Move {
-	fn score(&self) -> Score {
-		match self {
-			Move::Rock => 1,
-			Move::Paper => 2,
-			Move::Scissors => 3,
-		}
-	}
-
-	fn play_against(&self, other: Move) -> Outcome {
-		use self::Outcome::*;
-		match other {
-			_ if other == self.wins_against() => Win,
-			_ if other == self.loses_against() => Lose,
-			_ => Draw,
-		}
-	}
-
-	fn wins_against(&self) -> Move {
-		use Move::*;
-		match self {
-			Rock => Scissors,
-			Paper => Rock,
-			Scissors => Paper,
-		}
-	}
-
-	fn loses_against(&self) -> Move {
-		use Move::*;
-		match self {
-			Rock => Paper,
-			Paper => Scissors,
-			Scissors => Rock,
-		}
-	}
-}
-
-impl FromStr for Move {
-	type Err = ();
-
-	fn from_str(s: &str) -> Result<Self, Self::Err> {
-		Ok(match s {
-			"A" | "X" => Move::Rock,
-			"B" | "Y" => Move::Paper,
-			"C" | "Z" => Move::Scissors,
-			_ => return Err(()),
-		})
-	}
-}
-
-#[derive(PartialEq, Eq, Clone, Copy, Debug)]
-struct Turn {
-	their_move: Move,
-	our_move: Move,
-	desired_outcome: Outcome,
-}
-
-impl FromStr for Turn {
-	type Err = ();
-
-	fn from_str(s: &str) -> Result<Self, Self::Err> {
-		let (x, y) = s.split_once(' ').ok_or(())?;
-		Ok(Turn {
-			their_move: x.parse()?,
-			our_move: y.parse()?,
-			desired_outcome: y.parse()?,
-		})
-	}
-}
-
-impl Turn {
-	fn play(&self, strategy: Strategy) -> Score {
-		strategy.play(self)
-	}
-}
-
-#[derive(PartialEq, Eq, Clone, Copy, Debug)]
-enum Strategy {
-	PlayTurn,
-	ForceDesiredOutcome,
-}
-
-impl Strategy {
-	fn play(&self, turn: &Turn) -> Score {
-		use self::{Outcome::*, Strategy::*};
-		match self {
-			PlayTurn => turn.our_move.play_against(turn.their_move).score() + turn.our_move.score(),
-			ForceDesiredOutcome => {
-				let our_move = match turn.desired_outcome {
-					Win => turn.their_move.loses_against(),
-					Lose => turn.their_move.wins_against(),
-					Draw => turn.their_move,
-				};
-				our_move.score() + turn.desired_outcome.score()
-			}
-		}
-	}
+fn matchfixing_score(turn: &str) -> Result<u32, String> {
+	Ok(match turn {
+		"B X" => 1, // Paper    - Rock     : Lose
+		"C X" => 2, // Scissors - Paper    : Lose
+		"A X" => 3, // Rock     - Scissors : Lose
+		"A Y" => 4, // Rock     - Rock     : Draw
+		"B Y" => 5, // Paper    - Paper    : Draw
+		"C Y" => 6, // Scissors - Scissors : Draw
+		"C Z" => 7, // Scissors - Rock     : Win
+		"A Z" => 8, // Rock     - Paper    : Win
+		"B Z" => 9, // Paper    - Scissors : Win
+		_ => return Err(format!("Invalid pattern: '{turn}'")),
+	})
 }
 
 fn main() {
+	fn add(x: (u32, u32), y: (u32, u32)) -> (u32, u32) {
+		(x.0 + y.0, x.1 + y.1)
+	}
+
 	let (part1, part2) = stdin()
 		.lines()
 		.flatten()
-		.map(|line| line.parse::<Turn>().unwrap())
-		.map(|turn| {
-			(
-				turn.play(Strategy::PlayTurn),
-				turn.play(Strategy::ForceDesiredOutcome),
-			)
-		})
-		.fold((0u32, 0u32), |(x1, y1), (x2, y2)| {
-			(x1 + x2 as u32, y1 + y2 as u32)
-		});
+		.map(|ref turn| (score(turn).unwrap(), matchfixing_score(turn).unwrap()))
+		.fold((0, 0), add);
+
 	println!("Part 1: {part1}");
 	println!("Part 2: {part2}");
-}
-
-#[cfg(test)]
-mod tests {
-	use super::*;
-
-	#[test]
-	fn test() {
-		let turns: Vec<Turn> = vec!["A Y", "B X", "C Z"]
-			.into_iter()
-			.map(|x| x.parse().unwrap())
-			.collect();
-
-		assert_eq!(
-			vec![8, 1, 6],
-			turns
-				.iter()
-				.map(|turn| turn.play(Strategy::PlayTurn))
-				.collect::<Vec<_>>(),
-		);
-
-		assert_eq!(
-			vec![4, 1, 7],
-			turns
-				.iter()
-				.map(|turn| turn.play(Strategy::ForceDesiredOutcome))
-				.collect::<Vec<_>>(),
-		);
-	}
 }
