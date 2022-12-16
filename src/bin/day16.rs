8730489412ca5437ae9e5f2a143e2144c2cdbfc9
Jesse van Assen <jesse.v.assen@gmail.com>
2022-12-16 13:21:05 +0100

    Solve part 1 _somehow_

--- src/bin/day16.rs
+++ src/bin/day16.rs
@@ -1,5 +1,6 @@
-use std::io::stdin;
+use std::{collections::HashMap, io::stdin};
 
+use aoc2022::ByteSet;
 use regex::Regex;
 
 type Room = (
@@ -9,7 +10,65 @@ type Room = (
 
 fn main() {
 	let rooms = parse_input(stdin().lines().flatten());
-	dbg!(&rooms);
+
+	const TIME_LIMIT: u32 = 30;
+
+	let result = do_it_daddy(TIME_LIMIT, &rooms);
+	dbg!(result);
+}
+
+fn do_it_daddy(time_remaining: u32, rooms: &[Room]) -> u32 {
+	type Cache = HashMap<(u32, u8, ByteSet), u32>;
+
+	fn recurse(
+		time_remaining: u32,
+		current_room: u8,
+		visited: ByteSet,
+		rooms: &[Room],
+		cache: &mut Cache,
+	) -> u32 {
+		if time_remaining == 0 {
+			return 0;
+		}
+
+		let cache_key = (time_remaining, current_room, visited);
+
+		if let Some(&cached) = cache.get(&cache_key) {
+			return cached;
+		}
+
+		let time_remaining = time_remaining - 1;
+
+		let (flow, connected) = &rooms[current_room as usize];
+
+		let can_open_valve = *flow > 0 && !visited.contains(current_room);
+
+		let room_result = can_open_valve.then(|| {
+			let mut visited = visited;
+			visited.insert(current_room);
+			let room_produces = flow * time_remaining;
+			room_produces + recurse(time_remaining, current_room, visited, rooms, cache)
+		});
+
+		let result = connected
+			.iter()
+			.map(|&room| recurse(time_remaining, room, visited, rooms, cache))
+			.chain(room_result)
+			.max()
+			.unwrap_or(0);
+
+		cache.insert(cache_key, result);
+
+		result
+	}
+
+	recurse(
+		time_remaining,
+		0,
+		ByteSet::new(),
+		rooms,
+		&mut HashMap::new(),
+	)
 }
 
 fn parse_input(input: impl IntoIterator<Item = String>) -> Vec<Room> {
