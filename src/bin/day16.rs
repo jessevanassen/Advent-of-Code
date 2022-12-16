816a877773e7c84ead012bd56aff8dfe50840467
Jesse van Assen <jesse.v.assen@gmail.com>
2022-12-16 16:14:10 +0100

    Solve part 2 _more somehow_

--- src/bin/day16.rs
+++ src/bin/day16.rs
@@ -11,18 +11,18 @@ type Room = (
 fn main() {
 	let rooms = parse_input(stdin().lines().flatten());
 
-	const TIME_LIMIT: u32 = 30;
+	const TIME_LIMIT: u32 = 26;
 
 	let result = do_it_daddy(TIME_LIMIT, &rooms);
 	dbg!(result);
 }
 
 fn do_it_daddy(time_remaining: u32, rooms: &[Room]) -> u32 {
-	type Cache = HashMap<(u32, u8, ByteSet), u32>;
+	type Cache = HashMap<(u32, [u8; 2], ByteSet), u32>;
 
 	fn recurse(
 		time_remaining: u32,
-		current_room: u8,
+		current_rooms: [u8; 2],
 		visited: ByteSet,
 		rooms: &[Room],
 		cache: &mut Cache,
@@ -31,7 +31,7 @@ fn do_it_daddy(time_remaining: u32, rooms: &[Room]) -> u32 {
 			return 0;
 		}
 
-		let cache_key = (time_remaining, current_room, visited);
+		let cache_key = (time_remaining, current_rooms, visited);
 
 		if let Some(&cached) = cache.get(&cache_key) {
 			return cached;
@@ -39,23 +39,30 @@ fn do_it_daddy(time_remaining: u32, rooms: &[Room]) -> u32 {
 
 		let time_remaining = time_remaining - 1;
 
-		let (flow, connected) = &rooms[current_room as usize];
-
-		let can_open_valve = *flow > 0 && !visited.contains(current_room);
-
-		let room_result = can_open_valve.then(|| {
-			let mut visited = visited;
-			visited.insert(current_room);
-			let room_produces = flow * time_remaining;
-			room_produces + recurse(time_remaining, current_room, visited, rooms, cache)
+		let to_visit = current_rooms.map(|room| {
+			let (flow, connected) = &rooms[room as usize];
+			let can_open_valve = *flow > 0 && !visited.contains(room);
+			connected
+				.iter()
+				.map(|room| (*room, 0, visited))
+				.chain(can_open_valve.then(|| {
+					let mut visited = visited;
+					visited.insert(room);
+					let additional = time_remaining * flow;
+					(room, additional, visited)
+				}))
+				.collect::<Vec<_>>()
 		});
 
-		let result = connected
-			.iter()
-			.map(|&room| recurse(time_remaining, room, visited, rooms, cache))
-			.chain(room_result)
+		let result = combinations(&to_visit[0], &to_visit[1])
+			.filter(|(x, y)| !(x.0 == y.0 && x.1 > 0 && y.1 > 0))
+			.map(|(&x, &y)| ([x.0, y.0], x.1 + y.1, x.2.union(&y.2)))
+			.map(|(mut current_rooms, to_add, visited)| {
+				current_rooms.sort();
+				to_add + recurse(time_remaining, current_rooms, visited, rooms, cache)
+			})
 			.max()
-			.unwrap_or(0);
+			.unwrap();
 
 		cache.insert(cache_key, result);
 
@@ -64,11 +71,17 @@ fn do_it_daddy(time_remaining: u32, rooms: &[Room]) -> u32 {
 
 	recurse(
 		time_remaining,
-		0,
+		[0; 2],
 		ByteSet::new(),
 		rooms,
 		&mut HashMap::new(),
 	)
+}
+
+fn combinations<'a, T>(xs: &'a [T], ys: &'a [T]) -> impl Iterator<Item = (&'a T, &'a T)> + 'a {
+	(0..xs.len())
+		.flat_map(|i| (0..ys.len()).map(move |j| (i, j)))
+		.map(|(i, j)| (&xs[i], &ys[j]))
 }
 
 fn parse_input(input: impl IntoIterator<Item = String>) -> Vec<Room> {
@@ -129,4 +142,21 @@ fn parse_room_key(room: &str) -> u16 {
 		.into_iter()
 		.map(|c| c - b'A')
 		.fold(0, |acc, c| acc * 26 + c as u16)
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn test_combinations() {
+		let combinations = combinations(&[1, 2, 3], &[4, 5]).collect::<Vec<_>>();
+		assert_eq!(6, combinations.len());
+		assert_eq!((&1, &4), combinations[0]);
+		assert_eq!((&1, &5), combinations[1]);
+		assert_eq!((&2, &4), combinations[2]);
+		assert_eq!((&2, &5), combinations[3]);
+		assert_eq!((&3, &4), combinations[4]);
+		assert_eq!((&3, &5), combinations[5]);
+	}
 }
