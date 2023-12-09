14fcdbd92372315ea473b9cfb4a0cfcfdafaeac2
Jesse van Assen <jesse.v.assen@gmail.com>
2023-12-09 09:24:42 +0100

    [Day 09] Solve in-place

--- src/bin/day09.rs
+++ src/bin/day09.rs
@@ -1,7 +1,5 @@
 use std::io::stdin;
 
-use itertools::Itertools;
-
 fn main() {
 	let lines = stdin().lines().flatten();
 	let input = lines
@@ -21,26 +19,42 @@ fn main() {
 }
 
 fn solve<'a>(numbers: impl IntoIterator<Item = &'a i64>) -> i64 {
-	fn solve(numbers: Vec<i64>) -> i64 {
-		let mut patterns: Vec<Vec<i64>> = vec![numbers];
+	fn solve(mut numbers: Vec<i64>) -> i64 {
+		/// Calculate the differences between `numbers[i] and `numbers[i + 1]`, and stores it in
+		/// place of numbers[i].
+		/// Because the differences have one less item than the source they are based on, the last
+		/// item in `numbers` will be left as-is.
+		fn calculate_differences(numbers: &mut [i64]) {
+			for i in 0..(numbers.len() - 1) {
+				numbers[i] = numbers[i + 1] - numbers[i];
+			}
+		}
 
-		loop {
-			let differences = calculate_differences(patterns.last().unwrap());
-			patterns.push(differences);
+		/* This algorithm takes the numbers, and stores the differences in-place in the same Vec.
+		 * It keeps recursing on the differences, until the differences all become 0.
+		 * Because the differences have always one length less than the the numbers they are based
+		 * on, the last number of the differences is preserved.
+		 *
+		 * 1   3   6  10  15   21 |
+		 * 2   3   4   5   6 | 21
+		 * 1   1   1   1 | 6   21
+		 * 0   0   0 | 1   6   21
+		 *
+		 * When the iteration stops, the beginning of the Vec only contains `0` (the differences
+		 * compared to the previous iteration), and the ending numbers of all the prior iterations.
+		 * The solution can now be calculated by summing all numbers together. */
+		for end in (0..=numbers.len()).rev() {
+			let slice = &mut numbers[..(end - 1)];
 
-			if patterns.last().unwrap().iter().all_equal() {
+			calculate_differences(slice);
+
+			if slice.iter().all(|x| *x == 0) {
 				break;
 			}
 		}
 
-		patterns.iter().filter_map(|xs| xs.last()).sum::<i64>()
+		numbers.iter().sum()
 	}
 
 	solve(numbers.into_iter().copied().collect())
-}
-
-fn calculate_differences<'a>(numbers: impl IntoIterator<Item = &'a i64>) -> Vec<i64> {
-	let windows = numbers.into_iter().tuple_windows();
-	let differences = windows.map(|(x, y)| y - x);
-	differences.collect()
 }
