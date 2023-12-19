44ba74fcf556021aed27811c0d34e2193aeb66d3
Jesse van Assen <jesse.v.assen@gmail.com>
2023-12-19 21:08:56 +0100

    Solve by flattening the workflows to a graph

--- src/bin/day19.rs
+++ src/bin/day19.rs
@@ -1,25 +1,33 @@
-use std::{collections::HashMap, io::stdin, ops::Range, str::FromStr};
+use std::{
+	collections::HashMap,
+	io::{stdin, Read},
+	ops::Range,
+	str::FromStr,
+};
 
 use regex::Regex;
 
 fn main() {
-	let mut lines = stdin().lines().flatten();
+	let input = {
+		let mut buf = String::new();
+		stdin().read_to_string(&mut buf).unwrap();
+		buf
+	};
 
-	let (workflows, start_workflow) =
-		parse_workflows((&mut lines).take_while(|line| !line.is_empty()));
-	let start_workflow = &workflows[start_workflow];
-
-	let parts = parse_parts(lines);
+	let (rules, parts) = input.split_once("\n\n").unwrap();
+	let rules = parse_rules(rules);
+	let parts = parse_parts(parts);
 
 	let part1 = parts
-		.filter(|part| {
-			let mut rules = start_workflow;
+		.filter(|&part| {
+			let mut rule = rules[0];
+
 			'outer: loop {
-				let result = rules.iter().find_map(|rule| rule.apply(part)).unwrap();
+				let result = rule.apply(part);
 				match result {
 					RuleResult::End(v) => return v,
-					RuleResult::Jump(label) => {
-						rules = &workflows[label];
+					RuleResult::Jump(i) => {
+						rule = rules[i];
 						continue 'outer;
 					}
 				}
@@ -29,200 +37,125 @@ fn main() {
 		.sum::<usize>();
 	println!("Part 1: {part1}");
 
-	println!(
-		"Part 2: {}",
-		find_part_combination_count(&workflows, start_workflow)
-	);
+	println!("Part 2: {}", find_part_combination_count(&rules));
 }
 
-fn find_part_combination_count(workflows: &Vec<Workflow>, initial_workflow: &Workflow) -> usize {
-	fn find_part_combination_count(
-		mut pp: PossibleParts,
-		workflows: &Vec<Workflow>,
-		workflow: &Workflow,
-	) -> usize {
-		if pp.iter().all(|x| x.is_empty()) {
+fn find_part_combination_count(rules: &[Rule]) -> usize {
+	fn find_part_combination_count(pp: PossibleParts, rules: &[Rule], rule: Rule) -> usize {
+		if pp.is_empty() {
 			return 0;
 		}
 
 		let apply_rule_result = |pp: PossibleParts, rule_result: RuleResult| -> usize {
 			match rule_result {
 				RuleResult::End(false) => 0,
-				RuleResult::End(true) => pp.iter().map(|f| f.len()).product::<usize>(),
+				RuleResult::End(true) => pp.combinations(),
 				RuleResult::Jump(destination) => {
-					find_part_combination_count(pp, workflows, &workflows[destination])
+					find_part_combination_count(pp, rules, rules[destination])
 				}
 			}
 		};
 
-		let mut count = 0;
-
-		for rule in workflow.iter() {
-			match *rule {
-				Rule::Always(rule_result) => {
-					count += apply_rule_result(pp, rule_result);
-					break;
-				}
-				Rule::Conditional { condition, result } => {
-					let (current, remainder) = condition.split_possible_parts(&pp);
-					count += apply_rule_result(current, result);
-
-					pp = remainder;
-				}
-			}
+		match rule {
+			Rule::Always(result) => apply_rule_result(pp, result),
+			Rule::Conditional {
+				condition,
+				left,
+				right,
+			} => pp
+				.split(condition)
+				.into_iter()
+				.zip([left, right])
+				.map(|(pp, result)| apply_rule_result(pp, result))
+				.sum(),
 		}
-
-		count
 	}
 
-	find_part_combination_count(
-		[1..4001, 1..4001, 1..4001, 1..4001],
-		workflows,
-		initial_workflow,
-	)
+	find_part_combination_count(PossibleParts::new(), rules, rules[0])
 }
 
-fn parse_workflows(lines: impl Iterator<Item = String>) -> (Vec<Workflow>, usize) {
-	let lines = lines.collect::<Vec<_>>();
+fn parse_rules(input: &str) -> Vec<Rule> {
+	let mut lines = input.lines().collect::<Vec<_>>();
 
+	/* This makes working with the graph easier, because the starting rule will now always be the
+	 * first one in the resulting Vec. */
+	let start_position = lines
+		.iter()
+		.position(|line| line.starts_with("in{"))
+		.expect("Expected a start workflow");
+	lines.swap(0, start_position);
+
+	/* Map from the rule's label to it's position */
 	let label_map = lines
 		.iter()
-		.enumerate()
-		.map(|(i, line)| (line.split_once('{').unwrap().0, i))
+		.scan(0, |acc, line| {
+			let start = *acc;
+
+			let (label, rest) = line.split_once('{').unwrap();
+			let rule_count = rest.split(',').count();
+
+			*acc += rule_count;
+
+			Some((label, start))
+		})
 		.collect::<HashMap<_, _>>();
 
-	let workflows = lines
+	lines
 		.iter()
-		.map(|line| {
+		.flat_map(|line| {
 			let (_, rules) = line.split_once('{').unwrap();
 			let rules = &rules[..(rules.len() - 1)];
-
-			rules
-				.split(',')
-				.map(|rule| {
-					lazy_static::lazy_static! {
-						static ref PREDICATE_PATTERN: Regex = Regex::new(r"^([xmas])([<>])(\d+):(\w+)$").unwrap();
-					}
-
-					let parse_destination = |destination: &str| -> RuleResult {
-						match destination {
-							"A" => RuleResult::End(true),
-							"R" => RuleResult::End(false),
-							label => RuleResult::Jump(label_map[label]),
-						}
-					};
-
-					if let Some(captures) = PREDICATE_PATTERN.captures(rule) {
-						let field = captures.get(1).unwrap().as_str();
-						let field = Part::label_offset(field.as_bytes()[0]);
-
-						let operator: Operator = captures.get(2).unwrap().as_str().as_bytes()[0]
-							.try_into()
-							.unwrap();
-
-						let value = captures.get(3).unwrap().as_str().parse::<u16>().unwrap();
-
-						let destination = parse_destination(captures.get(4).unwrap().as_str());
-
-						Rule::Conditional {
-							condition: RuleCondition {
-								field,
-								value,
-								operator,
-							},
-							result: destination,
-						}
-					} else {
-						let destination = parse_destination(rule);
-						Rule::Always(destination)
-					}
-				})
-				.collect()
+			rules.split(',')
 		})
-		.collect();
-
-	let start = label_map["in"];
-
-	(workflows, start)
-}
-
-fn parse_parts(lines: impl Iterator<Item = String>) -> impl Iterator<Item = Part> {
-	lines.map(|line| line.parse().unwrap())
-}
-
-type Workflow = Vec<Rule>;
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-enum Operator {
-	GreaterThan,
-	LessThan,
-}
-
-impl TryFrom<u8> for Operator {
-	type Error = anyhow::Error;
-
-	fn try_from(value: u8) -> Result<Self, Self::Error> {
-		Ok(match value {
-			b'<' => Self::LessThan,
-			b'>' => Self::GreaterThan,
-			_ => anyhow::bail!("Unexpected character '{value}'"),
-		})
-	}
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-struct RuleCondition {
-	field: usize,
-	value: u16,
-	operator: Operator,
-}
-
-impl RuleCondition {
-	fn matches(&self, part: &Part) -> bool {
-		let RuleCondition {
-			field,
-			value,
-			operator,
-		} = *self;
-
-		let field_value = part.0[field];
-		match operator {
-			Operator::GreaterThan => field_value > value,
-			Operator::LessThan => field_value < value,
-		}
-	}
-
-	fn split_possible_parts(
-		&self,
-		possible_parts: &PossibleParts,
-	) -> (PossibleParts, PossibleParts) {
-		let RuleCondition {
-			field,
-			value,
-			operator,
-		} = *self;
-
-		match operator {
-			Operator::LessThan => {
-				let mut head = possible_parts.clone();
-				head[field] = head[field].start..value;
-
-				let mut remainder = possible_parts.clone();
-				remainder[field] = value..remainder[field].end;
-
-				(head, remainder)
+		.enumerate()
+		.map(|(i, rule)| {
+			lazy_static::lazy_static! {
+				static ref PREDICATE_PATTERN: Regex = Regex::new(r"^([xmas])([<>])(\d+):(\w+)$").unwrap();
 			}
-			Operator::GreaterThan => {
-				let (remainder, head) = RuleCondition {
-					field,
-					value: value + 1,
-					operator: Operator::LessThan,
+
+			let parse_destination = |destination: &str| -> RuleResult {
+				match destination {
+					"A" => RuleResult::End(true),
+					"R" => RuleResult::End(false),
+					label => RuleResult::Jump(label_map[label]),
 				}
-				.split_possible_parts(possible_parts);
-				(head, remainder)
+			};
+
+			if let Some(captures) = PREDICATE_PATTERN.captures(rule) {
+				let field = captures.get(1).unwrap().as_str();
+				let field = Part::label_offset(field.as_bytes()[0]);
+
+				let split_point = captures.get(3).unwrap().as_str();
+				let split_point = split_point.parse::<u16>().unwrap();
+
+				let destination = captures.get(4).unwrap().as_str();
+				let destination = parse_destination(destination);
+
+				let operator = captures.get(2).unwrap().as_str();
+				let operator = operator.as_bytes()[0];
+
+				let next_rule = RuleResult::Jump(i + 1);
+				let (split_point, left, right) = match operator {
+					b'<' => (split_point, destination, next_rule),
+					b'>' => (split_point + 1, next_rule, destination),
+					_ => panic!("Unexpected operator '{operator}'"),
+				};
+
+				Rule::Conditional {
+					condition: RuleCondition { field, split_point },
+					left,
+					right,
+				}
+			} else {
+				let destination = parse_destination(rule);
+				Rule::Always(destination)
 			}
-		}
-	}
+		})
+		.collect()
+}
+
+fn parse_parts(input: &str) -> impl Iterator<Item = Part> + '_ {
+	input.lines().map(|line| line.parse().unwrap())
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -230,27 +163,38 @@ enum Rule {
 	Always(RuleResult),
 	Conditional {
 		condition: RuleCondition,
-		result: RuleResult,
+		left: RuleResult,
+		right: RuleResult,
 	},
 }
 
 impl Rule {
-	fn matches(&self, part: &Part) -> bool {
-		match self {
-			Rule::Always(_) => true,
-			Rule::Conditional { condition, .. } => condition.matches(part),
+	fn apply(&self, part: Part) -> RuleResult {
+		match *self {
+			Self::Always(result) => result,
+			Self::Conditional {
+				condition,
+				left,
+				right,
+			} => {
+				if condition.evaluate(part) {
+					left
+				} else {
+					right
+				}
+			}
 		}
 	}
+}
 
-	fn destination(&self) -> RuleResult {
-		match self {
-			Rule::Always(result) => *result,
-			Rule::Conditional { result, .. } => *result,
-		}
-	}
-
-	fn apply(&self, part: &Part) -> Option<RuleResult> {
-		self.matches(part).then(|| self.destination())
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+struct RuleCondition {
+	field: usize,
+	split_point: u16,
+}
+impl RuleCondition {
+	fn evaluate(&self, part: Part) -> bool {
+		part.0[self.field] < self.split_point
 	}
 }
 
@@ -260,7 +204,30 @@ enum RuleResult {
 	Jump(usize),
 }
 
-type PossibleParts = [Range<u16>; 4];
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct PossibleParts([Range<u16>; 4]);
+
+impl PossibleParts {
+	fn new() -> Self {
+		Self([1..4001, 1..4001, 1..4001, 1..4001])
+	}
+
+	fn split(&self, RuleCondition { field, split_point }: RuleCondition) -> [Self; 2] {
+		let (mut left, mut right) = (self.0.clone(), self.0.clone());
+		left[field] = left[field].start..split_point;
+		right[field] = split_point..right[field].end;
+
+		[Self(left), Self(right)]
+	}
+
+	fn combinations(&self) -> usize {
+		self.0.iter().map(|r| r.len()).product()
+	}
+
+	fn is_empty(&self) -> bool {
+		self.combinations() == 0
+	}
+}
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 struct Part([u16; 4]);
